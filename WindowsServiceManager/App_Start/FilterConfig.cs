0334eb79a0d8ea2d260c53ac568c411aeed7a844
Marat Akzhigitov <m.akzhigitov@devinotele.com>
2015-09-09 19:57:41 +0300

    first commit

---
+++ WindowsServiceManager/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WindowsServiceManager
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WindowsServiceManager/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceProcess;
+using System.Web.Mvc;
+using SimpleImpersonation;
+
+namespace WindowsServiceManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IList<string> _serviceControllers;
+        readonly string _machineName;
+
+        public HomeController()
+        {
+            _machineName = ConfigurationManager.AppSettings["machineName"];
+            _serviceControllers = ConfigurationManager.AppSettings["services"].Split(';');
+        }
+
+        public ActionResult Index()
+        {
+            var serviceViewModels = _serviceControllers.Select(
+                serviceName => new ServiceViewModel
+                {
+                    ServiceName = serviceName,
+                    Status = GetStatus(serviceName)
+                }).ToList();
+
+            return View(serviceViewModels);
+        }
+
+        [HttpPost]
+        public ActionResult Stop(string serviceName)
+        {
+            using (LogonUser())
+            {
+                var serviceController = new ServiceController(serviceName, _machineName);
+                serviceController.Stop();
+                serviceController.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(10));
+            }
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Start(string serviceName)
+        {
+            using (LogonUser())
+            {
+                var serviceController = new ServiceController(serviceName, _machineName);
+                serviceController.Start();
+            }
+
+            return View("Index");
+        }
+
+        private string GetStatus(string serviceName)
+        {
+            using (LogonUser())
+            {
+                var serviceController = new ServiceController(serviceName, _machineName);
+                var status = serviceController.Status.ToString();
+
+                return status;
+            }
+        }
+
+        private static Impersonation LogonUser()
+        {
+            var domain = ConfigurationManager.AppSettings["domain"];
+            var username = ConfigurationManager.AppSettings["username"];
+            var password = ConfigurationManager.AppSettings["password"];
+            return Impersonation.LogonUser(domain, username, password, LogonType.NewCredentials);
+        }
+    }
+
+    public class ServiceViewModel
+    {
+        public string ServiceName { get; set; }
+        public string Status { get; set; }
+    }
+}
